b47c29ed4a6bc0a5b8d606bde6fcfa9d4cf49e97
Luis De Ávila <luis.deavila@urosario.edu.co>
2023-08-04 00:58:01 -0500

    Great performance boost by memo'd components

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,16 +1,16 @@
+import { memo } from "react";
 import { Image, View, useWindowDimensions } from "react-native"
 import pokeImages from "../assets/pokeImages";
 import { Pokemon } from "../types/Pokemon";
 
-function PokeCard({ pokemon }: { pokemon: Pokemon }) {
+const PokeCard = memo(({ pokemon }: { pokemon: Pokemon }) => {
   const dimension = useWindowDimensions();
 
   return (
     <View style={{ width: dimension.width / 2, aspectRatio: 1 }}>
       <Image source={pokeImages[pokemon.id]} resizeMode="contain" style={{ flex: 1, width: undefined, height: undefined }} />
     </View>
-    // <Image source={require('../pokeimages/ho-oh.gif')} style={{ width: dimension.width / 2, aspectRatio: 1 }} />
   )
-}
+})
 
-export default PokeCard
+export default PokeCard;
