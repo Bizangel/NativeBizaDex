8936c4e18d6dcfda56b1fa1ec8421f37a320def8
Luis De Ávila <luis.deavila@urosario.edu.co>
2023-08-13 13:31:36 -0500

    added preventing sleep while in dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import MainScreen from './components/MainScreen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { setIdleTimerDisabled } from 'react-native-idle-timer';
 
 function App() {
+
+  if (process.env.NODE_ENV === "development")
+    setIdleTimerDisabled(true);
+
   return (
     <GestureHandlerRootView>
       <MainScreen />
